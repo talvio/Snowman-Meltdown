ed70bafab01ff83cfc948af4f7fe4f5522073a43
Jukka Talvio <jukka.talvio@iki.fi>
2025-02-28 14:44:39 +0100

    initial ASCII art (stage 0) and word display (all underscores) appear.

--- snowman.py
+++ snowman.py
@@ -1,4 +1,5 @@
 import random
+from stages import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
@@ -8,6 +9,18 @@ def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    print(STAGES[mistakes])
+
+    word_to_show = "_" * len(secret_word)
+    print("Word: ", end="")
+    for c in secret_word:
+        if c in guessed_letters:
+            print(f"{c} ", end="")
+        else:
+            print("_ ", end="")
+    print()
+
 
 def play_game():
     secret_word = get_random_word()
@@ -16,8 +29,19 @@ def play_game():
 
     # TODO: Build your game loop here.
     # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
+    mistakes = 0
+    guessed_letters = set()
+    while True:
+        display_game_state(mistakes, secret_word, guessed_letters)
+        guess = "1"
+        while not guess.isalpha() or len(guess) != 1 or guess in guessed_letters:
+            guess = input("Guess a letter: ").lower()
+        print("You guessed:", guess)
+        guessed_letters.add(guess[0].lower())
+        if guess not in secret_word:
+            mistakes += 1
+        if mistakes > 3:
+            break
 
 
 if __name__ == "__main__":
